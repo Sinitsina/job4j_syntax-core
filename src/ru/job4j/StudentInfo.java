b0e77ca9e6555e9aa2b3c74fd3f2d797f9432f22
Sinitsina <sinitsina.olga.a@gmail.com>
2020-05-26 21:31:36 +0500

    3.3. Разбор первой программы. [#295911]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Olga Sinitsina");
+        System.out.println("05.10.1993");
+    }
+}
