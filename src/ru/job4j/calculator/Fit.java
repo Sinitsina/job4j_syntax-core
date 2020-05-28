b60fa83dbf7e73994b621be6fc77d40ad6a14a0d
Sinitsina <sinitsina.olga.a@gmail.com>
2020-05-28 20:12:24 +0500

    6. Идеальный вес. [#295891]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+        public static double manWeight(double height) {
+            double rsl = (height - 100) * 1.15;
+            return rsl;
+        }
+
+
+        public static double womanWeight(double height) {
+            double rsl = (height - 110) * 1.15;
+            return rsl;
+        }
+
+        public static void main(String[] args) {
+            double man = Fit.manWeight(100);
+            double woman = Fit.womanWeight(160);
+            System.out.println("Man 100 is " + man);
+            System.out.println("Woman 160 is " + woman);
+
+        }
+
+    }
+
