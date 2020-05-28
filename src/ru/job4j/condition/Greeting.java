e74e5942b4f8647f14d1b6199ec4e735c3728320
Sinitsina <sinitsina.olga.a@gmail.com>
2020-05-28 21:07:43 +0500

    9.1. String [#295901]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String idea2 = idea + "But I am a newbie.";
+        int year = 2020;
+        String result = idea2 + year;
+        System.out.println(result);
+
+
+    }
+}
