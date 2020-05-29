841a772bd53485fb40fe573286963c7d5878e9d2
Sinitsina <sinitsina.olga.a@gmail.com>
2020-05-29 20:38:34 +0500

    3. Тесты для определения расстояния между точками. [#295930]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -9,7 +9,7 @@ public class Point {
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
+        double result = Point.distance(10, 5, 5, 5);
         System.out.println("result (0, 0) to (2, 0) " + result);
     }
 }
