2e779e547220a8f4e69e0918e472fc939861efb3
Sinitsina <sinitsina.olga.a@gmail.com>
2020-05-29 20:52:50 +0500

    4. Тесты для стороны прямоугольника. [#295931]

---
+++ test/ru/job4j/condition/TrgAreaTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TrgAreaTest {
+
+    @Test
+    public void whenSides574ThenArea9() {
+        int a = 5;
+        int b = 7;
+        int c = 4;
+        double expected = 9.797958971132712;
+        double out = TrgArea.area(a, b, c);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
