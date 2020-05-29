2f97c5ad4a411f87803219ec781471093fe23a9a
Sinitsina <sinitsina.olga.a@gmail.com>
2020-05-29 20:11:32 +0500

    1. Модульные тесты. [#295928]

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert70RblThen1Dollar() {
+        int in = 70;
+        int expected = 1;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+}
