39cb3ea92f3e8e20d1d747b666c5a40c4256ec1d
Sinitsina <sinitsina.olga.a@gmail.com>
2020-06-03 20:24:00 +0500

    4.4. Максимум из трех чисел [#295860]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -5,14 +5,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class MultiMax {
 
     public int max(int first, int second, int third) {
-        int result = 0;
-        if (first > second && first > third) {
-            result = first;
-        } else if (first < second && second > third) {
-            result = second;
-        } else {
-            result = third;
-        }
+        int result = first > second ? first : second;
+        result = result > third ? result : third;
         return result;
     }
 }
