c046c4dc34ce3b351c9359ee79ca14fe2264a9ab
Sinitsina <sinitsina.olga.a@gmail.com>
2020-06-06 20:28:00 +0500

    5.8. JavaDoc. Code convention [#295953]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,11 +1,25 @@
 package ru.job4j.converter;
 
+/**
+ * Convert rubles to Euro and Dollars.
+ * @author Olga Sinitsina
+ */
 public class Converter {
+    /**
+     * Method rubleToEuro
+     * @param value - amount of rubles
+     * @return result of division value by 70 (exchange rate of Euro)
+     */
         public static int rubleToEuro(int value) {
             int rsl = value / 70;
             return rsl;
         }
 
+    /**
+     * Method rubleToDollar
+     * @param value - amount of rubles
+     * @return result of division value by 60 (exchange rate of Dollar)
+     */
         public static int rubleToDollar(int value) {
             int rsl = value / 60;
             return rsl;
