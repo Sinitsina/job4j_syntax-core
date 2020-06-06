460e7ebb9f23869c35f9b281cf6d5ecaee2b1fae
Sinitsina <sinitsina.olga.a@gmail.com>
2020-06-06 19:36:33 +0500

    5.5. Простое число [#295945]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,17 +2,12 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        int i = 2;
-        if (number == 1) {
-            prime = false;
-        }
-        while (number > i) {
+        boolean prime = number > 1;
+        for (int i = 2; number > i; i++) {
             if (number % i == 0) {
                 prime = false;
                 break;
             }
-            i++;
         }
         return prime;
     }
