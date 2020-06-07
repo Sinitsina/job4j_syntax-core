008c4648509b6402f3d0816f313d09cafa9604e2
Sinitsina <sinitsina.olga.a@gmail.com>
2020-06-07 15:29:55 +0500

    6.0.2. Заполнение массива. [#295879]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,12 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Petr Petrov";
+        names[2] = "Maxim Maximov";
+        names[3] = "Semen Sidorov";
+        System.out.print(names[0] + "\n" + names[1] + "\n" + names[2] + "\n" + names[3]);
     }
 }
