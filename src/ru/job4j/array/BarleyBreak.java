30f3cb8d0bbd08847cca053c5c8e29bcf8205ea3
Sinitsina <sinitsina.olga.a@gmail.com>
2020-06-12 17:56:50 +0500

    6.6.1. Двухмерный массив. Вывод на консоль. [#295885]

---
+++ src/ru/job4j/array/BarleyBreak.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class BarleyBreak {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        int counter = 1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[i][j] = counter;
+                counter++;
+            }
+        }
+    }
+}
