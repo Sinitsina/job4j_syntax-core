0eaae531722e0ed80fe40be8c603304d9257354b
Sinitsina <sinitsina.olga.a@gmail.com>
2020-06-12 17:11:49 +0500

    6.4.1. Слово заканчивается на ... [#295871]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -4,11 +4,8 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int i = 1; i <= post.length; i++) {
-            if (word[word.length -i] == post[post.length-i]) {
-                result = true;
-            } else {
+            if (word[word.length -i] != post[post.length-i]) {
                 result = false;
-                break;
             }
         }
         return result;
