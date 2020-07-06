df454b372d40d2f8510fef7338fe4f2d0e71c931
Sinitsina <sinitsina.olga.a@gmail.com>
2020-07-06 14:25:52 +0500

    2.2. Рефакторинг - Площадь треугольника. [#296031]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -9,22 +9,18 @@ public class PointTest {
 
     @Test
     public void whenCoordinates10555ThenDistance5() {
-        int x1 = 10;
-        int y1 = 5;
-        int x2 = 5;
-        int y2 = 5;
+        Point first = new Point(10, 5);
+        Point second = new Point(5, 5);
         double expected = 5.0;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = first.distance(second);
         Assert.assertEquals(expected, out, 0.01);
     }
     @Test
     public void whenCoordinates1111ThenDistance5() {
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 1;
-        int y2 = 1;
+        Point first = new Point(1, 1);
+        Point second = new Point(1, 1);
         double expected = 0.0;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = first.distance(second);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
