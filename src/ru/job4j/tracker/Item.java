bac6c07b4ad846da4886d73fd3e91b5e77497ba0
Sinitsina <sinitsina.olga.a@gmail.com>
2020-07-06 19:12:53 +0500

    2. Перегрузка конструктора. [#296046]

---
+++ src/ru/job4j/tracker/Item.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+public class Item {
+    String id;
+    String name;
+
+    public Item() {
+    }
+
+    public Item(String id) {
+    }
+
+    public Item(String is, String name) {
+    }
+}
