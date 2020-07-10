888cb7ceda411a64291f5c4736bd41b1aa10c480
Sinitsina <sinitsina.olga.a@gmail.com>
2020-07-10 10:20:11 +0500

    8. Что такое валидация? [#296058]

--- src/ru/job4j/tracker/Tracker.java
+++ src/ru/job4j/tracker/Tracker.java
@@ -49,19 +49,24 @@ public class Tracker {
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         item.setId(id);
-        items[index] = item;
-        return index != -1 ? true : false;
+        boolean rsl = index != -1;
+        if (rsl) {
+            items[index] = item;
+        }
+        return rsl;
     }
 
     public boolean delete(int id) {
         int index = indexOf(id);
         int start = index + 1;
-        int distPos = index;
         int size1 = size - index;
-        System.arraycopy(items, start, items, index, size1);
-        items[size - 1] = null;
-        size--;
-        return index != -1 ? true : false;
+        boolean rsl = index != -1;
+        if (rsl) {
+            System.arraycopy(items, start, items, index, size1);
+            items[size - 1] = null;
+            size--;
+        }
+        return rsl;
     }
 
 }
