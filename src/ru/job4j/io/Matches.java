142c45e80d5520a8daf1517adc5d63c7f464ec60
Sinitsina <sinitsina.olga.a@gmail.com>
2020-07-12 16:51:59 +0500

    2. Scanner и чтение числа из консоли. [#295992]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -6,20 +6,24 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int matches = 11;
+        System.out.println("Enter number from 1 to 3");
+        boolean player = true;
         while(matches >= 0) {
-            int player1 = Integer.valueOf(input.nextLine());;
-            if (matches - player1 > 0) {
-                System.out.println(matches - player1);
-                matches -= player1;
-                int player2 = Integer.valueOf(input.nextLine());;
-                if (matches - player2 > 0) {
-                    System.out.println(matches - player2);
-                    matches -= player2;
-                } else if (matches - player2 == 0) {
+            int quantity = Integer.valueOf(input.nextLine());
+            while (quantity > 3) {
+                System.out.println("Enter number from 1 to 3");
+                quantity = Integer.valueOf(input.nextLine());
+            }
+            if (matches - quantity > 0) {
+                System.out.println(matches - quantity);
+                matches -= quantity;
+                player = !player;
+            } else if (matches - quantity == 0) {
+                if (player == true) {
+                    System.out.println("Player 1 is winner");
+                } else if (player == false) {
                     System.out.println("Player 2 is winner");
                 }
-            } else if (matches - player1 == 0) {
-                System.out.println("Player 1 is winner");
             }
         }
     }
