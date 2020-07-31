bc6d22172eb76f2c2cab9fa9b8ffdb5811020a7a
Sinitsina <sinitsina.olga.a@gmail.com>
2020-07-31 10:29:09 +0500

    0.4. Пользовательские исключения. [#296012]

---
+++ src/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if(value[i].equals(key)) {
+                rsl=i;
+                break;
+            }
+        }
+        if (rsl < 0) {
+                throw new ElementNotFoundException("Element wasn't found");
+            }
+        return rsl;
+    }
+
+    public static void main(String[]args){
+        try{
+            String[] numbers = {"one", "two", "three"};
+            int result = FindEl.indexOf(numbers, "t8o");
+            System.out.println(result);
+        }
+        catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
