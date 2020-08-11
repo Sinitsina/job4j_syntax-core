b8e576bb92d449d4a5eb53527c65122960f770f0
Sinitsina <sinitsina.olga.a@gmail.com>
2020-08-11 13:53:26 +0500

    0.5. Иерархия исключений и множественный catch. [#296013]

--- src/ru/job4j/ex/ElementNotFoundException.java
+++ src/ru/job4j/ex/ElementNotFoundException.java
@@ -1,6 +1,8 @@
 package ru.job4j.ex;
 
 public class ElementNotFoundException extends Exception {
-    public ElementNotFoundException(String s) {
-    }
+    public ElementNotFoundException(String message) {
+    super(message);
 }
+}
+
